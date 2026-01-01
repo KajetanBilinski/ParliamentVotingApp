a2b66f97d8c6a1075dfe562ba7748ba080cfaa2c
KajetanBilinski <Kaje.bile@gmail.com>
2026-01-01 23:11:27 +0100

    test

--- ParliamentVotingApp/Enums/VoteType.cs
+++ ParliamentVotingApp/Enums/VoteType.cs
@@ -2,7 +2,7 @@
 
 namespace ParliamentVotingApp.Enums;
 
-[JsonConverter(typeof(JsonStringEnumConverter))]
+[JsonConverter(typeof(JsonStringEnumConverter))] 
 public enum VoteType
 {
     YES, NO, ABSTAIN, NO_VOTE, ABSENT, VOTE_VALID, VOTE_INVALID, PRESENT
